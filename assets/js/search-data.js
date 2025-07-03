d559ec98177ce5766001dc62175b122fe9ab01b0
amir-aghdam <73444725+amir-aghdam@users.noreply.github.com>
2025-07-03 06:32:02 +0000

    Deploying to gh-pages from @ amir-aghdam/amir-aghdam.github.io@c24b2f93917a8cfd0f187f4ab53d4ff09a37892a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,14 +10,14 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-blog",
-          title: "blog",
+          title: "Blog",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/blog/";
           },
         },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "My publications in reversed chronological order.",
           section: "Navigation",
           handler: () => {
@@ -41,41 +41,36 @@ ninja.data = [{
           section: "News",},{id: "news-my-most-recent-work-is-published-on-arxiv-in-collaboration-with-lmu-munich-project-page",
           title: '📢 My most recent work is published on arXiv in collaboration with LMU...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-actalign-zero-shot-fine-grained-video-classification",
+          title: 'ActAlign: Zero-Shot Fine-Grained Video Classification',
+          description: "A zero-shot framework that uses LLM-generated sub-action scripts and sequence alignment to classify fine-grained actions in video without any video–text supervision.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-beyond-confidence-evidential-learning-for-robust-classification",
+          title: 'Beyond Confidence: Evidential Learning for Robust Classification',
+          description: "A deep learning project focused on uncertainty-aware classification using Dirichlet-based evidential models improving F1 score by %10.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-related-work-co-pilot",
+          title: 'Related Work Co-pilot',
+          description: "An AI-powered assistant for drafting related work sections, combining large language models and arXiv with a human-in-the-loop approach for structured, iterative refinement.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-django-image-processor",
+          title: 'Django Image Processor',
+          description: "A cloud-hosted web app for applying image filters in real-time using Django and Pillow.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-3d-u-net",
+          title: '3D U-Net',
+          description: "a clean Pytorch implementation of 3D U-Net for volumetric medical images with a customized data pipeline.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-3d-brain-tumor-segmentation",
+          title: '3D Brain Tumor Segmentation',
+          description: "The PyTorch implementation of the first-place winning model on the BRATS dataset for 3D brain tumor segmentation using MRI scans.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
             },},{
         id: 'social-email',
         title: 'email',
